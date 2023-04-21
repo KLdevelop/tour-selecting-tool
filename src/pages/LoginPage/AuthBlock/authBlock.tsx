c803709ffa109c5f4de97a1fe559c3ca1903c481
KLdevelop <drapdrop1@gmail.com>
2023-04-21 20:05:10 +0300

    button component

--- src/pages/LoginPage/AuthBlock/authBlock.tsx
+++ src/pages/LoginPage/AuthBlock/authBlock.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './authBlock.module.scss';
+import { Button } from 'src/components/Button';
 
 export const AuthBlock = () => {
   return (
     <div className={styles.authBlock}>
       <h1>Auth</h1>
+      <Button>Text</Button>
     </div>
   );
 };
