fca60ac419cef601a61e12df81574106cb3b3c5f
houstonia~ <uwuowo733@gmail.com>
2023-04-21 20:02:00 +0300

    inp

--- src/pages/LoginPage/AuthBlock/authBlock.tsx
+++ src/pages/LoginPage/AuthBlock/authBlock.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+// import { InputText } from 'src/components/Input/component/InputText/InputText';
 import styles from './authBlock.module.scss';
+import lockIcon from 'src/assets/InputIcon/lockIcon.png';
 
 export const AuthBlock = () => {
   return (
     <div className={styles.authBlock}>
       <h1>Auth</h1>
+      <div className={styles.inputBlock}>
+        <div className={styles.inputBlockLogo}>
+          <img src={lockIcon} />
+        </div>
+        <input type="text" className={styles.inputBlockInp} />
+      </div>
     </div>
   );
 };
