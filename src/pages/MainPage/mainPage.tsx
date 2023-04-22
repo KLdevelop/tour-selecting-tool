5bb9435667ff73a2f062993f9dc4cedd44bd1b8d
KLdevelop <drapdrop1@gmail.com>
2023-04-22 11:52:38 +0300

    added header component

--- src/pages/MainPage/mainPage.tsx
+++ src/pages/MainPage/mainPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Header } from 'src/components';
 
 import styles from './mainPage.module.scss';
 
 export const MainPage = () => {
   return (
     <div className={styles.mainPage}>
+      <Header />
       <ul>
         <li>
           <Link to="/login/authorization">Войти</Link>
