b70a1aed61404513fe3b6c81d43f73c0ab6722d3
KLdevelop <drapdrop1@gmail.com>
2023-04-22 21:05:28 +0300

    new export modules

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { Button } from './Button';
 export { CustomInput } from './CustomInput';
 export { Input } from './Inp';
 export { Header } from './Header';
+export { MainImageCard } from './MainImageCard';
+export { SearchInput } from './SearchInput';
