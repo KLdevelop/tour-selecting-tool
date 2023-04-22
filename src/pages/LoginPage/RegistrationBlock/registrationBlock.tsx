f6e49120af080059737e4fd033f6abac1fcb7ede
KLdevelop <drapdrop1@gmail.com>
2023-04-22 10:34:08 +0300

    changed registration page

--- src/pages/LoginPage/RegistrationBlock/registrationBlock.tsx
+++ src/pages/LoginPage/RegistrationBlock/registrationBlock.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
+import { lockIcon, userIcon, emailIcon } from 'src/assets/InputIcon';
+import { CustomInput as Input, Button } from 'src/components';
 import styles from './registrationBlock.module.scss';
-import userIcon from 'src/assets/InputIcon/userIcon.png';
-import lockIcon from 'src/assets/InputIcon/lockIcon.png';
-import emailIcon from 'src/assets/InputIcon/emailIcon.png';
-import { Input } from 'src/components/Inp';
-import { Button } from 'src/components/Button';
 
 export const RegistrationBlock = () => {
   return (
     <div className={styles.registrationBlock}>
-      <div className={styles.inputBlock}>
-        <div className={styles.inputBlockLogo}>
-          <img src={emailIcon} />
-        </div>
-        <Input placeholder="почта" />
-      </div>
-      <div className={styles.inputBlock}>
-        <div className={styles.inputBlockLogo}>
-          <img src={userIcon} />
-        </div>
-        <Input placeholder="никнейм" />
-      </div>
-      <div className={styles.inputBlock}>
-        <div className={styles.inputBlockLogo}>
-          <img src={lockIcon} />
-        </div>
-        <Input placeholder="пароль" />
-      </div>
+      <Input icon={emailIcon} placeholder="почта" />
+      <Input icon={userIcon} placeholder="никнейм" />
+      <Input icon={lockIcon} type="password" placeholder="пароль" />
       <Button>Войти</Button>
     </div>
   );
