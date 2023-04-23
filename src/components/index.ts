ed7934d9273ffd4b4c4cf59e28dc86e3799ddc71
houstonia~ <uwuowo733@gmail.com>
2023-04-23 16:10:07 +0300

    product card

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { Input } from './Inp';
 export { Header } from './Header';
 export { MainImageCard } from './MainImageCard';
 export { SearchInput } from './SearchInput';
+export { ProductCard } from './ProductCard';
--- src/pages/OverviewPage/overviewPage.tsx
+++ src/pages/OverviewPage/overviewPage.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
+import { ProductCard } from 'src/components';
+import alpPic from 'src/assets/Images/alp.png';
 import styles from './overviewPage.module.scss';
 
 export const OverviewPage = () => {
-  return (<div className={styles.overviewPage}></div>);
-}
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  // let test = {
+  //   image: 'string',
+  //   tag: 'string',
+  //   location: 'string',
+  //   title: 'string',
+  //   description: 'string',
+  //   price: 0,
+  //   date: 'string',
+  // };
+  return (
+    <div className={styles.overviewPage}>
+      <div className={styles.overviewPage_product}>
+        {arr.map((i) => (
+          <ProductCard
+            key={i}
+            image={alpPic}
+            tag="Походы"
+            location="Россия, Камчатка"
+            title="Все лучшее на Камчатке? Без палаток? Да!"
+            description="В самый разгар лета 2023 года, мы подготовили для вас уникаcnwnfownroВ самый разгар лета 2023 года, мы подготовили для вас уникаcnwnfownroВ самый разгар лета 2023 года, мы подготовили для вас уникаcnwnfownroВ самый разгар лета 2023 года, мы подготовили для вас уникаcnwnfownro"
+            price={1_445}
+            date="2 - 8 июл."
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
