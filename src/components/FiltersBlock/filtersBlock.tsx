9a3787cbcc08c28b9b43d5602d482c8d640ad6c1
KLdevelop <drapdrop1@gmail.com>
2023-04-24 17:47:03 +0300

    added elements for touTypes and state for range

--- src/components/FiltersBlock/filtersBlock.tsx
+++ src/components/FiltersBlock/filtersBlock.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DropdownBlock } from './DropdownBlock';
-import styles from './filtersBlock.module.scss';
 import { SplittedInput } from './SplittedInput';
+import styles from './filtersBlock.module.scss';
 
 export const FiltersBlock = () => {
+  const [durationRange, setDuration] = useState({ from: 0, to: 36 });
+  const [priceRange, setPrice] = useState({ from: 0, to: 100000 });
+
   return (
     <div className={styles.filtersBlock}>
       <DropdownBlock title="Длительность">
-        <SplittedInput leftLabel="От (дней)" rightLabel="До (дней)" />
+        <SplittedInput
+          leftLabel="От (дней)"
+          rightLabel="До (дней)"
+          range={durationRange}
+          setValue={setDuration}
+        />
       </DropdownBlock>
-      <DropdownBlock title="Тип тура"></DropdownBlock>
+      <DropdownBlock title="Тип тура">
+        <div className={styles.tourTypes}>
+          <div className={styles.option}>
+            <input type="checkbox" id="first" />
+            <label htmlFor="first">Походы</label>
+          </div>
+          <div className={styles.option}>
+            <input type="checkbox" id="second" />
+            <label htmlFor="second">Горящие туры</label>
+          </div>
+          <div className={styles.option}>
+            <input type="checkbox" id="third" />
+            <label htmlFor="third">От местных</label>
+          </div>
+        </div>
+      </DropdownBlock>
       <DropdownBlock title="Цена">
-        <SplittedInput leftLabel="От" rightLabel="До" />
+        <SplittedInput leftLabel="От" rightLabel="До" range={priceRange} setValue={setPrice} />
       </DropdownBlock>
     </div>
   );
