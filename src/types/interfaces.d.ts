1ee3270af37bd79e64d2297d0fc156da88bd0278
KLdevelop <drapdrop1@gmail.com>
2023-04-25 17:08:01 +0300

    cards type

--- src/types/interfaces.d.ts
+++ src/types/interfaces.d.ts
@@ -2,3 +2,19 @@ export interface Range {
   from: number;
   to: number;
 }
+
+export interface Card {
+  id: number;
+  imageUrl: string;
+  tag: string;
+  location: string;
+  name: string;
+  fromdate: string;
+  todate: string;
+  content: string;
+  price: number;
+}
+
+export interface CardsResponse {
+  cards: Card[];
+}
