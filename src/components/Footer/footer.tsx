3a0d95b7aa9534d41f18bd07e96e9d1bf263497c
KLdevelop <drapdrop1@gmail.com>
2023-04-25 09:20:20 +0300

    added scroll to footer from header

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.scss';
 
 export const Footer = () => {
   return (
-    <footer className={styles.footer}>
+    <footer id="footer" className={styles.footer}>
       <div className={styles.mainContent}>
         <div className={styles.contacts}>
           <p className={styles.blockName}>Контакты</p>
