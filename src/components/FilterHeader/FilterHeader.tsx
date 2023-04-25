21e8bbcfc5efec7f476dfab6e699cf11c2409f70
KLdevelop <drapdrop1@gmail.com>
2023-04-25 11:28:48 +0300

    fixed styles

--- src/components/FilterHeader/FilterHeader.tsx
+++ src/components/FilterHeader/FilterHeader.tsx
@@ -5,13 +5,13 @@ import typeImg from 'src/assets/filterIcon/typeImage.png';
 import styles from './FilterHeader.module.scss';
 
 export const FilterHeader = () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+  const arr = ['Все', 'Походы', 'Горящие туры', 'Исторические места', 'От местных'];
   return (
     <div className={styles.filterHeader}>
       <DataPlaceFilter />
       <div className={styles.filterHeader_cards}>
-        {arr.map((_, ind) => (
-          <TypeCard image={typeImg} type="Все" key={ind} />
+        {arr.map((card) => (
+          <TypeCard image={typeImg} type={card} key={card} />
         ))}
       </div>
     </div>
